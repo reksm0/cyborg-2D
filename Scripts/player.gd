b09c176d71487997565101cc07780ade4e1c4845
reksm0 <manamisaha7777@gmail.com>
2026-06-23 13:42:29 +0530

    attacks and all added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,43 +1,69 @@
 extends CharacterBody2D
 
-
 const SPEED = 170.0
 const JUMP_VELOCITY = -450.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_attacking = false
 
 func _physics_process(delta):
-	# Add the gravity.
+
+	# Attack
+	if Input.is_action_just_pressed("attack") and !is_attacking:
+		start_attack()
+
+	# Prevent movement while attacking
+	if is_attacking:
+		move_and_slide()
+		return
+
+	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction (input_axis) and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var input_axis = Input.get_axis("left", "right")
+
 	if input_axis:
 		velocity.x = input_axis * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
 	update_animations(input_axis)
 	reset_player_position()
 
+
+func start_attack():
+	is_attacking = true
+	velocity.x = 0
+
+	animated_sprite_2d.play("attack")
+
+	await animated_sprite_2d.animation_finished
+
+	is_attacking = false
+
+
 func update_animations(input_axis):
-	
+
+	if is_attacking:
+		return
+
 	if input_axis > 0:
 		animated_sprite_2d.flip_h = false
 	elif input_axis < 0:
 		animated_sprite_2d.flip_h = true
-	if  is_on_floor():
+
+	if is_on_floor():
 		if input_axis != 0:
 			animated_sprite_2d.play("run")
 		else:
@@ -47,11 +73,8 @@ func update_animations(input_axis):
 			animated_sprite_2d.play("fall")
 		else:
 			animated_sprite_2d.play("jump")
-	
+
 
 func reset_player_position():
-	# used only for demonstration purposes.
 	if global_position.y > 500:
 		global_position = Vector2(100, 100)
-		
-	
