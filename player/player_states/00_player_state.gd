43033e5afaf694975a47e6a846e4c15f3b52ebbf
reksm0 <manamisaha7777@gmail.com>
2026-07-02 15:30:04 +0530

    title menu
    main menu almost done

--- player/player_states/00_player_state.gd
+++ player/player_states/00_player_state.gd
@@ -4,6 +4,29 @@ class_name PlayerState extends Node
 
 var player : Player
 var next_state : PlayerState
+#region /// state references
+ #reference to all other state
+#endregion
+
+#to initilise state
+func _init() -> void:
+	pass
+	
+#things that might happen when u enter a state
+func _enter() -> void:
+	pass
+	
+func _exit() -> void:
+	pass
+	
+func handle_input(_enter: InputEvent) -> void:
+	return next_state
+
+
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
