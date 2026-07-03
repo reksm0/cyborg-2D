dd63dffbf1bf128921e2ccfc35e1aeee0bebca64
goutam-ux <goutam.nath.official@gmail.com>
2026-07-04 04:43:00 +0530

    added the elevator system
    only the graphics need to be polished

---
+++ Scripts/elevator_atrium.gd
@@ -0,0 +1,69 @@
+extends Node2D
+@onready var elevator_menu: CanvasLayer = $"Elevator Menu"
+@onready var player: Player = $player
+@onready var main_elevator: Node2D = $"Main elevator"
+
+@onready var development_marker: Marker2D = $"Development Marker"
+@onready var assembly_marker: Marker2D = $"Assembly Marker"
+@onready var testing_marker: Marker2D = $"Testing Marker"
+
+enum Floor {
+	DEVELOPMENT,
+	ASSEMBLY,
+	TESTING,
+	GROUND
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	elevator_menu.floor_selected.connect(_on_floor_selected)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_floor_selected(floor):
+	print("Travelling to ", floor)
+	elevator_menu.visible = false
+	player.controls_enabled = false
+	player.animated_sprite_2d.visible = false
+	player.collision_shape_2d.disabled = true
+	print("Travelling...")
+	
+	
+	var target_position: Vector2
+
+	match floor:
+		Floor.DEVELOPMENT:
+			target_position = development_marker.global_position
+		
+		Floor.ASSEMBLY:
+			target_position = assembly_marker.global_position
+		
+		Floor.TESTING:
+			target_position = testing_marker.global_position
+	
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(
+		player,
+		"global_position",
+		target_position,
+		.7
+	)
+	tween.tween_property(
+		main_elevator,
+		"global_position",
+		target_position,
+		.7
+	)
+	await tween.finished
+	
+	elevator_menu.current_floor = floor
+	elevator_menu.update_menu()
+	
+	print("Arrived!")
+	
+	player.animated_sprite_2d.visible = true
+	player.controls_enabled = true
+	player.collision_shape_2d.disabled = false
